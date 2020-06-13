51dd0052fe6ca771c8c0b858f56f4f25d5df059a
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-13 00:54:45 +0000

    layout update

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,14 +25,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      {/*<Header siteTitle={data.site.siteMetadata.title} />*/}
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <button>+233 54 135 6456</button>
-    <h1>FIND YOUR DREAM HOME TODAY</h1>
-    <p>Contact Axtella for all your Construction & Real Estate Services.</p>
-    <button>Contact us</button>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <button>+233 54 135 6456</button>
+      <h1>FIND YOUR DREAM HOME TODAY</h1>
+      <p>Contact Axtella for all your Construction & Real Estate Services.</p>
+      <button>Contact us</button>
+     </div>
+    </div>
+    <div>
+    </div>
 
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
