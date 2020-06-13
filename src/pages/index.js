6096d9553b2452f3ec415759cea331a7bafb46b1
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-13 01:07:48 +0000

    hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -16,9 +17,6 @@ const IndexPage = () => (
       <button>Contact us</button>
      </div>
     </div>
-    <div>
-    </div>
-
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
