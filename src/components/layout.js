394516afebffbc63c10c892eb8803fb75c82c265
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-13 01:52:51 +0000

    Hero css

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+//import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+{/*  const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -23,7 +23,8 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
+*/} 
+ return (
     <>
       {/*<Header siteTitle={data.site.siteMetadata.title} />*/}
       <div>
