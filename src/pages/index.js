44f61cb4c22599ec16c1e1059ddcbbf4284e5641
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-16 23:25:19 +0000

    hero form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,65 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 
-const IndexPage = () => (
+
+const IndexPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  return(
   <Layout>
     <SEO title="Home" />
     <div className="Hero">
       <div className="HeroGroup">
-      <button className="ContactButton">+233 54 135 6456</button>
+      <button className="ContactButton">
+      <i class="material-icons">phone_iphone</i>
+      +233 54 135 6456</button>
       <h1>FIND YOUR DREAM HOME TODAY</h1>
       <p>Contact Axtella for all your Construction & Real Estate Services.</p>
       <button className="ContactUsButton">Contact us</button>
      </div>
+
+     <form className="Form">
+     <p>Need Help?</p>
+     <h2>MESSAGE US</h2>
+
+     <label>
+      <input
+        name='name'
+        value={name}
+        placeholder="Your Name"
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      </label>
+
+      <label>
+      <input
+        name='email'
+        value={email}
+        placeholder="Your Email"
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+     </label>
+
+      <label className="Message">
+      <input
+        name='message'
+        value={message}
+        placeholder="Message"
+        onChange={(e) => setMessage(e.target.value)}
+        required
+      />
+     </label>
+      <button>Send Message</button>
+     </form>
     </div>
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -24,6 +68,6 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+)}
 
 export default IndexPage
