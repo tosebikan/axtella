8ef8e5b68acd260c451a970ca794f0d3fa570e21
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-16 15:53:33 +0000

    responsive header

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,52 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, {useState, useEffect} from "react"
 import './Header.css';
 
 
 const Header = ({ siteTitle }) => {
-  const [isExpanded, toggleExpansion] = React.useState(false)
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
   return(
   <div className="Header">
   <Link to="/"><img src={require('../images/logo.png')} width='100' alt={siteTitle} className="Logo"/></Link>
-    <div className="HeaderGroup">
+  <button onClick={toggleNav} className="Burger">
+    &#9776;
+  </button>
+  {(!isSmallScreen || isNavVisible) && (
+    <nav className="Nav">
       <Link to="/about">Home</Link>
       <Link to="/about-us<">About us</Link>
       <Link to="/properties">Properties</Link>
       <Link to="/agents">Agents</Link>
       <Link to="/faq">FAQ</Link>
       <Link to="/contact">Contact</Link>
-    </div>
+    </nav>
+  )}
+
   </div>
 )}
 
