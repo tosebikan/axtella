dfd29acc5968633796dfb63c25ecc5677e0f1a74
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-23 13:52:15 +0000

    about page created

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'gatsby';
+import Layout from "../components/layout";
+import Header from '../components/header';
+
+const About = () => (
+  <Layout>
+  <div className="About-Hero">
+    <div className="About-title">
+      <h1>About us</h1>
+    </div>
+  </div>
+  </Layout>
+)
+
+export default About;
