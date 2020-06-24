0dd8c78763dd400d5e39765e9f78d3556fed087f
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-24 14:14:50 +0000

    added property details

---
+++ src/pages/property-details.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../components/layout";
+
+
+const PropertyDetails = () => (
+  <Layout>
+  <div className="About-Hero">
+    <div className="About-title">
+      <h1>About us</h1>
+    </div>
+  </div>
+  </Layout>
+)
+
+export default PropertyDetails;
