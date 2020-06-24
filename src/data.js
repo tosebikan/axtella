e3aa0e8fd90aaa1efabfe212ce8f11b7b2ab6966
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-24 14:48:51 +0000

    data update

--- src/data.js
+++ src/data.js
@@ -1,8 +1,77 @@
  const data={
 properties:[
   {
-    
-  }
+    id: 1,
+    tag: 'For Sale',
+    title: 'Modern Apartment',
+    price: '100,000',
+    address: '123 Fifth Avenue, East-Legon',
+    bedrooms: 3,
+    Baths: 4,
+    size: 150,
+    image: './images/home1.jpg',
+    featured: true
+  },
+  {
+    id: 2,
+    tag: 'For Sale',
+    title: 'Family Home',
+    price: 800,
+    address: '123 Fifth Avenue, East-Legon',
+    bedrooms: 3,
+    Baths: 4,
+    size: 150,
+    image: './images/home2.jpg',
+    featured: true
+  },
+  {
+    id: 3,
+    tag: 'For Rent',
+    title: 'Family Home',
+    price: '900/Mo',
+    address: '123 Fifth Avenue, East-Legon',
+    bedrooms: 3,
+    Baths: 4,
+    size: 150,
+    image: './images/home2.jpg',
+    featured: true
+  },
+  {
+    id: 4,
+    tag: 'For Sale',
+    title: 'Luxury Apartment',
+    price: '120,000',
+    address: '123 Fifth Avenue, East-Legon',
+    bedrooms: 3,
+    Baths: 4,
+    size: 150,
+    image: './images/home2.jpg',
+    featured: true
+  },
+  {
+    id: 5,
+    tag: 'For Rent',
+    title: 'Family Homes',
+    price: '900/Mo',
+    address: '123 Fifth Avenue, East-Legon',
+    bedrooms: 3,
+    Baths: 4,
+    size: 150,
+    image: './images/home2.jpg',
+    featured: false
+  },
+  {
+    id: 6,
+    tag: 'For Sale',
+    title: 'Luxury Apartment',
+    price: '120,000',
+    address: '123 Fifth Avenue, East-Legon',
+    bedrooms: 3,
+    Baths: 4,
+    size: 150,
+    image: './images/home2.jpg',
+    featured: false
+  },
 ]
  }
 
