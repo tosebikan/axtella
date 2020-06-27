a9938b24f9e64e9fb849e8a72a19d083861dedb6
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-27 18:33:03 +0000

    property details update

--- src/pages/property-details.js
+++ src/pages/property-details.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import Layout from "../components/layout";
+import data from '../data';
+
+import './property-details.css';
 
 
 const PropertyDetails = (props) => {
-  //console.log(props.match.params.id)
+  console.log('m',props.id)
+  const property = data.properties.find((x) => x.id === props.id)
+  //console.log('data', data.properties)
+  console.log('property',property)
   return(
   <Layout>
-  <div className="About-Hero">
-    <div className="About-title">
-      <h1>About us</h1>
+  <div className="Details">
+    <div className="Prop-Details">
+      <img alt="home1" src={require('../images/test.jpg')}/>
+      <div className="Prop-Info">
+        <div className="Prop-Info1">
+        <h1>{property.title}</h1>
+        <h3>{property.address}</h3>
+        </div>
+        <div className="Prop-Info2">
+        <h1>Price: ${property.price}</h1>
+        <h3>{property.tag}</h3>
+        </div>
+      </div>
+    </div>
+    <div className="Map">
+    <img alt="home1" src={require('../images/test.jpg')}/>
+    {/*  <img src={property.image} alt="home1"/> */}
     </div>
   </div>
+
   </Layout>
 )}
 
