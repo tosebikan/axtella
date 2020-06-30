23a86e3d6645b10590fdc346afd49aaba9a0e5db
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-30 22:04:42 +0000

    About page updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Layout from "../components/layout";
+import './About.css';
 
 
 const About = () => (
   <Layout>
   <div className="About-Hero">
-    <div className="About-title">
-      <h1>About us</h1>
-    </div>
+
+  <div className="About1">
+  <h1>About Us</h1>
+  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  </p>
+  <p>
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+  <div className="About-Button">
+  <button>
+  <i className="material-icons">place</i>
+  Contact Us
+  </button>
+  </div>
+  </div>
+
+  <div className="About-Image">
+  <img src={require('../images/about.jpg')}alt="aboutus"/>
+  </div>
+
   </div>
   </Layout>
 )
