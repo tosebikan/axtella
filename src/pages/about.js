56990ced8b124912e4eed40cf700111a0249cd16
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-30 22:25:47 +0000

    about page update

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,6 @@ import './About.css';
 const About = () => (
   <Layout>
   <div className="About-Hero">
-
   <div className="About1">
   <h1>About Us</h1>
   <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -23,7 +22,7 @@ const About = () => (
   </div>
 
   <div className="About-Image">
-  <img src={require('../images/about.jpg')}alt="aboutus"/>
+  <img src={require('../images/about.jpg')} alt="aboutus"/>
   </div>
 
   </div>
