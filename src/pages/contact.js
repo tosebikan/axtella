0c272398573081088b111cb03a212a7f9cacc4eb
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-01 11:31:02 +0000

    contact page update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Layout from "../components/layout";
+import './contact.css';
 
 
 const Contact = () => (
   <Layout>
-  <div className="About-Hero">
-    <div className="About-title">
-      <h1>Contacts</h1>
+  <div className="Contact-Hero">
+    <div className='Contact-Map-Section'>
+    <img src={require('../images/map.jpg')} alt="contact map"/>
+      <div className="Contact-Address">
+      <i className="material-icons">place</i>
+      <p>Coastal Spintex, Accra</p>
+      </div>
     </div>
   </div>
   </Layout>
