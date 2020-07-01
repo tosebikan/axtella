510f44489ffa05f33eedef2206d9d214e9ebec1d
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-01 13:12:29 +0000

    Maps component

---
+++ src/pages/Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Maps extends React.Component{
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
