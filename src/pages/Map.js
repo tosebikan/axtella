f87dcb5ce2eaf808cf27b16ff2d2d6e941546dbb
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-02 09:22:05 +0000

    maps

---
+++ src/pages/Map.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const Address = ({ text }) => <div style={{fontSize:21, display: 'flex', flexDirection: 'row', alignItems: 'center'}}><i style={{color: 'red', fontSize:30}}className="material-icons">place</i>{text}</div>;
+
+export default class Map extends React.Component{
+  static defaultProps = {
+    center: {
+      lat: 5.636864,
+      lng: -0.097823
+    },
+    zoom: 15
+  };
+  render(){
+    return(
+      <div style={{ height: '100%', width: '100%' }}>
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDlWWwqqGvDkTJYReDJm0LGcuWJGJo_c_M' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          options={{
+      styles: [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }]
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }]
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }]
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }]
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }]
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }]
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }]
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }]
+        }
+      ]
+    }}
+        >
+        <Address
+            lat={5.636864}
+            lng={-0.097823}
+            text="Coastal"/>
+        </GoogleMapReact>
+
+      </div>
+    )
+  }
+}
