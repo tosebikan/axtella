bd2204d12997ca22c99741dbc7078bcccccd44e2
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-02 18:50:37 +0000

    Faq page added

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import Layout from "../components/layout";
+import './Faq.css';
 
 
 const Faq = () => (
   <Layout>
-  <div className="About-Hero">
-    <div className="About-title">
-      <h1>FAQ</h1>
+  <div className="Faq-Hero">
+    <div className="Faq-Hero-Section">
+      <div className="Faq-Hero-title">
+        <p>FAQ</p>
+        <h1>Frequently Asked Questions</h1>
+      </div>
+    </div>
+
+    <div className="Faq-Quest-Section">
+      <div className="Faq-Quest">
+        <h3>Should i rent or buy a home?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div className="Faq-Quest">
+        <h3>How long does it take to buy a home?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div className="Faq-Quest">
+        <h3>Am i ready to buy?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div className="Faq-Quest">
+        <h3>Am i ready to rent?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div className="Faq-Quest">
+        <h3>Am i ready to buy?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div className="Faq-Quest">
+        <h3>Am i ready to rent?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+
     </div>
   </div>
   </Layout>
