9b3e737bcad7e888b2db552e10d8c51542295a31
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-02 09:17:02 +0000

    properties page update

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Layout from "../components/layout";
+import './properties.css';
 
 
 const Properties = () => (
   <Layout>
-  <div className="About-Hero">
-    <div className="About-title">
-      <h1>Properties</h1>
+  <div className="Prop-Hero">
+    <div className="Prop-Banner">
+    {/*<img src={require('../images/propBanner.jpg')} alt="prop-banner"/>*/}
+      <div className="Prop-Hero-Text">
+      <p>We find the best<br/>
+    houses for you.</p>
+      </div>
+    </div>
+
+    <div className="Prop-Card">
+    <img src={require('../images/home1.jpg')} alt="nice-house"/>
+
     </div>
   </div>
   </Layout>
