9faf9afddc05d890ad430ad3a838d1783c67693b
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-03 13:01:53 +0000

    all properties mapped from data

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -7,7 +7,8 @@ import data from '../data';
 
 const Properties = () => {
   const property = data.properties.filter((x) => x.type === 'house')
-  console.log('p',property)
+  const lands = data.properties.filter((x) => x.type === "land")
+  console.log('p',lands)
   return(
   <Layout>
   <div className="Prop-Hero">
@@ -24,8 +25,8 @@ const Properties = () => {
 
 
     <div className="Prop-Container">
-    {data.properties.map((prop, id) =>
-      <div className="Prop-Section">
+    {property.map((prop, id) =>
+      <div className="Prop-Section" key={id}>
       <img className="Center" src={prop.image} alt="nice-house"/>
         <div className="Prop-Details">
           <div className="Prop-Details-Top">
@@ -53,61 +54,21 @@ const Properties = () => {
     </div>
 
     <div className="Prop-Container">
-    <div className="Prop-Section">
-    <img className="Center" src={require('../images/land1.jpg')} alt="nice-house"/>
-      <div className="Prop-Details">
-        <div className="Prop-Details-Top">
-        <p>1 Acre</p>
-        <p>$ 100,000</p>
-        </div>
-        <div className="Prop-Details-Bottom">
-        <p>Coastal Spintex, Accra Ghana</p>
-        <h4> For Sale</h4>
-        </div>
-      </div>
-    </div>
-
-      <div className="Prop-Section">
-      <img className="Center" src={require('../images/land2.jpg')} alt="nice-house"/>
-        <div className="Prop-Details">
-          <div className="Prop-Details-Top">
-          <p>10 Plots</p>
-          <p>$ 200,000</p>
-          </div>
-          <div className="Prop-Details-Bottom">
-          <p>Coastal Spintex, Accra Ghana</p>
-          <h4> For Sale</h4>
+      {lands.map((land, id) =>
+        <div className="Prop-Section">
+        <img className="Center" src={land.image} alt="nice-house"/>
+          <div className="Prop-Details">
+            <div className="Prop-Details-Top">
+            <p>{land.title}</p>
+            <p>${land.price}</p>
+            </div>
+            <div className="Prop-Details-Bottom">
+            <p>{land.address}</p>
+            <h4>{land.tag}</h4>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="Prop-Section">
-      <img className="Center" src={require('../images/land3.jpg')} alt="nice-house"/>
-        <div className="Prop-Details">
-          <div className="Prop-Details-Top">
-          <p>2 Acres</p>
-          <p>$ 5,000</p>
-          </div>
-          <div className="Prop-Details-Bottom">
-          <p>Coastal Spintex, Accra Ghana</p>
-          <h4> For Sale</h4>
-          </div>
-        </div>
-      </div>
-
-      <div className="Prop-Section">
-      <img className="Center" src={require('../images/land4.jpg')} alt="nice-house"/>
-        <div className="Prop-Details">
-          <div className="Prop-Details-Top">
-          <p>10 Plots</p>
-          <p>$ 5,000</p>
-          </div>
-          <div className="Prop-Details-Bottom">
-          <p>Coastal Spintex, Accra Ghana</p>
-          <h4> For Rent</h4>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
 
   </div>
