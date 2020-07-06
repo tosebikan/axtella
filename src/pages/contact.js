dee643c05d11350cec225e62cafa32ba31efb3bf
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-06 12:49:14 +0000

    form submit implementation with formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,6 +9,30 @@ const Contact = () => {
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+
+  const submitForm = (ev) => {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus("SUCCESS")
+        setName('')
+        setEmail('')
+        setMessage('')
+        setPhone('')
+      } else {
+        setStatus("ERROR")
+      }
+    };
+    xhr.send(data);
+  }
 
   return(
   <Layout>
@@ -38,7 +62,11 @@ const Contact = () => {
       <p>Mon - Fri: 8:00 am - 6pm</p>
       </div>
     </div>
-    <form className="Contact-Info-Form">
+    <form className="Contact-Info-Form"
+    onSubmit={submitForm}
+    action="https://formspree.io/mqkyylrk"
+    method="POST"
+    >
       <label className="Name">
         <input
         name="name"
@@ -58,7 +86,7 @@ const Contact = () => {
       </label>
       <label className="Email">
         <input
-        name="email"
+        name="_replyto"
         value={email}
         placeholder="email address"
         onChange={(e) => setEmail(e.target.value)}
@@ -67,12 +95,17 @@ const Contact = () => {
       </label>
       <label className="Contact-Message">
         <textarea
+        name="message"
         value={message}
         placeholder="message"
         onChange={(e) => setMessage(e.target.value)}
         required/>
       </label>
       <button className="Form-Button">send message</button>
+      { status === "SUCCESS" ? <p>Thanks</p> : null}
+      {
+       status === "ERROR" && <p>Ooops! There was an error.</p>
+     }
     </form>
 
   </div>
