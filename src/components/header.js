3f1884752f433e4922c4492df4a59f6aef4a37a8
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-13 18:34:59 +0000

    updates

--- src/components/header.js
+++ src/components/header.js
@@ -1,54 +1,85 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, {useState, useEffect} from "react"
-import './Header.css';
-
+import React, { useState, useEffect } from "react"
+import "./Header.css"
 
 const Header = ({ siteTitle }) => {
-  const [isNavVisible, setNavVisibility] = useState(false);
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  {
+    /*const [isNavVisible, setNavVisibility] = useState(false)
+  const [isSmallScreen, setIsSmallScreen] = useState(false)*/
+  }
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 700px)");
-    mediaQuery.addListener(handleMediaQueryChange);
-    handleMediaQueryChange(mediaQuery);
+  {
+    /*useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)")
+    mediaQuery.addListener(handleMediaQueryChange)
+    handleMediaQueryChange(mediaQuery)
 
     return () => {
-      mediaQuery.removeListener(handleMediaQueryChange);
-    };
-  }, []);
+      mediaQuery.removeListener(handleMediaQueryChange)
+    }
+  }, [])
 
   const handleMediaQueryChange = mediaQuery => {
     if (mediaQuery.matches) {
-      setIsSmallScreen(true);
+      setIsSmallScreen(true)
     } else {
-      setIsSmallScreen(false);
+      setIsSmallScreen(false)
     }
-  };
+  }*/
+  }
 
   const toggleNav = () => {
-    setNavVisibility(!isNavVisible);
-  };
+    // setNavVisibility(!isNavVisible)
+    console.log(document.querySelector(".Nav").classList.toggle("open"))
+  }
 
-  return(
-  <div className="Header">
-  <Link to="/"><img src={require('../images/logo.png')} width='150' alt={siteTitle} className="Logo"/></Link>
-  <button onClick={toggleNav} className="Burger">
-    &#9776;
-  </button>
-  {(!isSmallScreen || isNavVisible) && (
-    <nav className="Nav">
-      <Link to="/">Home</Link>
-      <Link to="/about/">About</Link>
-      <Link to="/properties/">Properties</Link>
-      {/*<Link to="/agents">Agents</Link>*/}
-      <Link to="/faq">FAQ</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
-  )}
-
-  </div>
-)}
+  return (
+    <div className="Header">
+      <Link to="/">
+        <img
+          src={require("../images/logo.png")}
+          width="150"
+          alt={siteTitle}
+          className="Logo"
+        />
+      </Link>
+      <button onClick={toggleNav} className="Burger">
+        &#9776;
+      </button>
+      {/* (!isSmallScreen || isNavVisible) && (
+        <nav className="Nav">
+          <Link to="/">Home</Link>
+          <Link to="/about/">About</Link>
+          <Link to="/properties/">Properties</Link>
+          <Link to="/agents">Agents</Link>
+          <Link to="/faq">FAQ</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      ) */}
+      <nav className="Nav">
+        <Link to="/" onClick={toggleNav}>
+          Home
+        </Link>
+        <Link to="/about/" onClick={toggleNav}>
+          About
+        </Link>
+        <Link to="/properties/" onClick={toggleNav}>
+          Properties
+        </Link>
+        <Link to="/agents" onClick={toggleNav}>
+          Agents
+        </Link>
+        <Link to="/faq" onClick={toggleNav}>
+          FAQ
+        </Link>
+        <Link to="/contact" onClick={toggleNav}>
+          Contact
+        </Link>
+      </nav>
+    </div>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
