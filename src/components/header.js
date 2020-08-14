02543a44a7ca8913fb076be6c1bd3b65b85f629a
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-14 18:21:39 +0000

    updates

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,9 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState, useEffect } from "react"
+import React from "react"
 import "./Header.css"
 
 const Header = ({ siteTitle }) => {
-  {
-    /*const [isNavVisible, setNavVisibility] = useState(false)
-  const [isSmallScreen, setIsSmallScreen] = useState(false)*/
-  }
-
-  {
-    /*useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 700px)")
-    mediaQuery.addListener(handleMediaQueryChange)
-    handleMediaQueryChange(mediaQuery)
-
-    return () => {
-      mediaQuery.removeListener(handleMediaQueryChange)
-    }
-  }, [])
-
-  const handleMediaQueryChange = mediaQuery => {
-    if (mediaQuery.matches) {
-      setIsSmallScreen(true)
-    } else {
-      setIsSmallScreen(false)
-    }
-  }*/
-  }
-
   const toggleNav = () => {
     // setNavVisibility(!isNavVisible)
     console.log(document.querySelector(".Nav").classList.toggle("open"))
@@ -47,16 +22,6 @@ const Header = ({ siteTitle }) => {
       <button onClick={toggleNav} className="Burger">
         &#9776;
       </button>
-      {/* (!isSmallScreen || isNavVisible) && (
-        <nav className="Nav">
-          <Link to="/">Home</Link>
-          <Link to="/about/">About</Link>
-          <Link to="/properties/">Properties</Link>
-          <Link to="/agents">Agents</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
-      ) */}
       <nav className="Nav">
         <Link to="/" onClick={toggleNav}>
           Home
